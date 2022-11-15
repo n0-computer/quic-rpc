f91b841a8fdcea4b3e45d04649a096eb5375add6
Ruediger Klaehn <rklaehn@protonmail.com>
2022-11-15 16:45:49 +0200

    Some unboxing

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-use std::{result, pin::Pin, io};
-
-use anyhow::Context;
-use futures::{future::BoxFuture, Future, FutureExt, Sink, Stream, StreamExt, SinkExt, channel::mpsc, stream::BoxStream};
-use serde::{Serialize, Deserialize, de::DeserializeOwned};
-use tokio_serde::{formats::SymmetricalBincode, SymmetricallyFramed};
-use tokio_util::codec::{FramedWrite, FramedRead, LengthDelimitedCodec};
+use futures::{
+    channel::mpsc, future::BoxFuture, Future, FutureExt, Sink, SinkExt, Stream, StreamExt,
+};
 use pin_project::pin_project;
-
+use quinn::{RecvStream, SendStream};
+use serde::{de::DeserializeOwned, Serialize};
+use std::{io, pin::Pin, result, task::Poll};
+use tokio_serde::{formats::SymmetricalBincode, SymmetricallyFramed};
+use tokio_util::codec::{FramedRead, FramedWrite, LengthDelimitedCodec};
+mod mem_channel;
+mod quinn_channel;
 
 trait InteractionPattern {}
 
@@ -38,7 +40,10 @@ struct WrapNoError<S> {
 impl<S: Stream + Unpin> Stream for WrapNoError<S> {
     type Item = Result<S::Item, NoError>;
 
-    fn poll_next(self: Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> std::task::Poll<Option<Self::Item>> {
+    fn poll_next(
+        self: Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Option<Self::Item>> {
         let mut this = self.project();
         match this.inner.poll_next_unpin(cx) {
             std::task::Poll::Ready(Some(x)) => std::task::Poll::Ready(Some(Ok(x))),
@@ -48,37 +53,40 @@ impl<S: Stream + Unpin> Stream for WrapNoError<S> {
     }
 }
 
-enum Never {}
-type NoSink = futures::sink::Drain<Never>;
-type OneImmediateResponse<T> = futures::stream::Once<futures::future::Ready<T>>;
-
 trait Channel<Req, Res> {
+    /// Sink type
     type Sink: Sink<Req, Error = Self::SendError>;
+    /// Stream type
     type Stream: Stream<Item = result::Result<Res, Self::RecvError>>;
-    /// Error you might get when opening a new stream
-    type OpenBiError;
-    /// Error you might get when waiting for new streams
-    type AcceptBiError;
     /// Error you might get while sending messages from a stream
     type SendError;
     /// Error you might get while receiving messages from a stream
     type RecvError;
-
-    type OpenBiFuture<'a>: Future<Output = result::Result<(Self::Sink, Self::Stream), Self::OpenBiError>> + 'a where Self: 'a;
+    /// Error you might get when opening a new stream
+    type OpenBiError;
+    /// Future returned by open_bi
+    type OpenBiFuture<'a>: Future<Output = result::Result<(Self::Sink, Self::Stream), Self::OpenBiError>>
+        + 'a
+    where
+        Self: 'a;
+    /// Open a bidirectional stream
     fn open_bi(&mut self) -> Self::OpenBiFuture<'_>;
-
-    type AcceptBiFuture<'a>: Future<Output = result::Result<(Self::Sink, Self::Stream), Self::AcceptBiError>> + 'a where Self: 'a;
+    /// Error you might get when waiting for new streams
+    type AcceptBiError;
+    /// Future returned by accept_bi
+    type AcceptBiFuture<'a>: Future<Output = result::Result<(Self::Sink, Self::Stream), Self::AcceptBiError>>
+        + 'a
+    where
+        Self: 'a;
+    /// Accept a bidirectional stream
     fn accept_bi(&mut self) -> Self::AcceptBiFuture<'_>;
 }
 
-type MemSocket<Req, Res> = (
-    mpsc::Sender<Req>,
-    WrapNoError<mpsc::Receiver<Res>>,
-);
+type MemSocket<Req, Res> = (mpsc::Sender<Req>, WrapNoError<mpsc::Receiver<Res>>);
 
 type QuinnSocket<Req, Res> = (
-    Pin<Box<dyn Sink<Req, Error = io::Error> + Send>>,
-    BoxStream<'static, result::Result<Res, io::Error>>,
+    QuinnLengthDelimitedBincodeSink<Req>,
+    QuinnLengthDelimitedBincodeStream<Res>,
 );
 
 struct MemChannel<Req, Res> {
@@ -86,10 +94,100 @@ struct MemChannel<Req, Res> {
     sink: mpsc::Sender<MemSocket<Res, Req>>,
 }
 
-impl<Req: Serialize + Send + 'static, Res: DeserializeOwned + Send + 'static> Channel<Req, Res> for quinn::Connection {
-    type Sink = Pin<Box<dyn Sink<Req, Error = Self::SendError> + Send>>;
+/// A sink that wraps a quinn SendStream with length delimiting and bincode
+#[pin_project]
+struct QuinnLengthDelimitedBincodeSink<T> {
+    #[pin]
+    inner: tokio_serde::Framed<
+        FramedWrite<SendStream, LengthDelimitedCodec>,
+        T,
+        T,
+        SymmetricalBincode<T>,
+    >,
+}
 
-    type Stream = Pin<Box<dyn Stream<Item = result::Result<Res, Self::RecvError>> + Send>>;
+impl<T> QuinnLengthDelimitedBincodeSink<T> {
+    fn new(
+        inner: tokio_serde::Framed<
+            FramedWrite<SendStream, LengthDelimitedCodec>,
+            T,
+            T,
+            SymmetricalBincode<T>,
+        >,
+    ) -> Self {
+        Self { inner }
+    }
+}
+
+impl<T: Serialize> Sink<T> for QuinnLengthDelimitedBincodeSink<T> {
+    type Error = io::Error;
+
+    fn poll_ready(
+        self: Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Result<(), Self::Error>> {
+        self.project().inner.poll_ready_unpin(cx)
+    }
+
+    fn start_send(self: Pin<&mut Self>, item: T) -> Result<(), Self::Error> {
+        self.project().inner.start_send_unpin(item)
+    }
+
+    fn poll_flush(
+        self: Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Result<(), Self::Error>> {
+        self.project().inner.poll_flush_unpin(cx)
+    }
+
+    fn poll_close(
+        self: Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Result<(), Self::Error>> {
+        self.project().inner.poll_close_unpin(cx)
+    }
+}
+
+/// A sink that wraps a quinn SendStream with length delimiting and bincode
+#[pin_project]
+struct QuinnLengthDelimitedBincodeStream<T> {
+    #[pin]
+    inner: tokio_serde::SymmetricallyFramed<
+        FramedRead<RecvStream, LengthDelimitedCodec>,
+        T,
+        SymmetricalBincode<T>,
+    >,
+}
+
+impl<T> QuinnLengthDelimitedBincodeStream<T> {
+    fn new(
+        inner: tokio_serde::SymmetricallyFramed<
+            FramedRead<RecvStream, LengthDelimitedCodec>,
+            T,
+            SymmetricalBincode<T>,
+        >,
+    ) -> Self {
+        Self { inner }
+    }
+}
+
+impl<T: DeserializeOwned> Stream for QuinnLengthDelimitedBincodeStream<T> {
+    type Item = result::Result<T, io::Error>;
+
+    fn poll_next(
+        self: Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Option<Self::Item>> {
+        self.project().inner.poll_next_unpin(cx)
+    }
+}
+
+impl<Req: Serialize + Send + 'static, Res: DeserializeOwned + Send + 'static> Channel<Req, Res>
+    for quinn::Connection
+{
+    type Sink = QuinnLengthDelimitedBincodeSink<Req>;
+
+    type Stream = QuinnLengthDelimitedBincodeStream<Res>;
 
     type OpenBiError = quinn::ConnectionError;
 
@@ -104,7 +202,10 @@ impl<Req: Serialize + Send + 'static, Res: DeserializeOwned + Send + 'static> Ch
     fn open_bi(&mut self) -> Self::OpenBiFuture<'_> {
         let this = self.clone();
         async move {
-            let conn: result::Result<(quinn::SendStream, quinn::RecvStream), quinn::ConnectionError> = quinn::Connection::open_bi(&this).await;
+            let conn: result::Result<
+                (quinn::SendStream, quinn::RecvStream),
+                quinn::ConnectionError,
+            > = quinn::Connection::open_bi(&this).await;
             let (send, recv) = conn?;
             // turn chunks of bytes into a stream of messages using length delimited codec
             let send = FramedWrite::new(send, LengthDelimitedCodec::new());
@@ -113,18 +214,23 @@ impl<Req: Serialize + Send + 'static, Res: DeserializeOwned + Send + 'static> Ch
             let recv = SymmetricallyFramed::new(recv, SymmetricalBincode::<Res>::default());
             let send = SymmetricallyFramed::new(send, SymmetricalBincode::<Req>::default());
             // box so we don't have to write down the insanely long type
-            let send: Pin<Box<dyn Sink<Req, Error = std::io::Error> + Send>> = Box::pin(send);
-            let recv: BoxStream<'static, result::Result<Res, std::io::Error>> = Box::pin(recv);
+            let send = QuinnLengthDelimitedBincodeSink::new(send);
+            let recv = QuinnLengthDelimitedBincodeStream::new(recv);
             Ok((send, recv))
-        }.boxed()
+        }
+        .boxed()
     }
 
-    type AcceptBiFuture<'a> = BoxFuture<'a, result::Result<QuinnSocket<Req, Res>, Self::AcceptBiError>>;
+    type AcceptBiFuture<'a> =
+        BoxFuture<'a, result::Result<QuinnSocket<Req, Res>, Self::AcceptBiError>>;
 
     fn accept_bi(&mut self) -> Self::AcceptBiFuture<'_> {
         let this = self.clone();
         async move {
-            let conn: result::Result<(quinn::SendStream, quinn::RecvStream), quinn::ConnectionError> = quinn::Connection::accept_bi(&this).await;
+            let conn: result::Result<
+                (quinn::SendStream, quinn::RecvStream),
+                quinn::ConnectionError,
+            > = quinn::Connection::accept_bi(&this).await;
             let (send, recv) = conn?;
             // turn chunks of bytes into a stream of messages using length delimited codec
             let send = FramedWrite::new(send, LengthDelimitedCodec::new());
@@ -133,50 +239,103 @@ impl<Req: Serialize + Send + 'static, Res: DeserializeOwned + Send + 'static> Ch
             let recv = SymmetricallyFramed::new(recv, SymmetricalBincode::<Res>::default());
             let send = SymmetricallyFramed::new(send, SymmetricalBincode::<Req>::default());
             // box so we don't have to write down the insanely long type
-            let send: Pin<Box<dyn Sink<Req, Error = std::io::Error> + Send>> = Box::pin(send);
-            let recv: BoxStream<'static, result::Result<Res, std::io::Error>> = Box::pin(recv);
+            let send = QuinnLengthDelimitedBincodeSink::new(send);
+            let recv = QuinnLengthDelimitedBincodeStream::new(recv);
             Ok((send, recv))
-        }.boxed()
+        }
+        .boxed()
     }
 }
 
 struct PeerDropped;
 
-impl<Req: Send + 'static, Res: Send + 'static> Channel<Req, Res> for MemChannel<Req, Res> {
+#[pin_project]
+struct MemOpenBiFuture<'a, Req, Res> {
+    #[pin]
+    inner: futures::sink::Send<'a, mpsc::Sender<MemSocket<Res, Req>>, MemSocket<Res, Req>>,
+    res: Option<MemSocket<Req, Res>>,
+}
 
+impl<'a, Req, Res> MemOpenBiFuture<'a, Req, Res> {
+    fn new(
+        inner: futures::sink::Send<'a, mpsc::Sender<MemSocket<Res, Req>>, MemSocket<Res, Req>>,
+        res: MemSocket<Req, Res>,
+    ) -> Self {
+        Self {
+            inner,
+            res: Some(res),
+        }
+    }
+}
+
+impl<'a, Req, Res> Future for MemOpenBiFuture<'a, Req, Res> {
+    type Output = result::Result<MemSocket<Req, Res>, mpsc::SendError>;
+
+    fn poll(
+        self: Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Self::Output> {
+        let mut this = self.project();
+        match this.inner.poll_unpin(cx) {
+            Poll::Ready(Ok(())) => this
+                .res
+                .take()
+                .map(|x| Poll::Ready(Ok(x)))
+                .unwrap_or(Poll::Pending),
+            Poll::Ready(Err(cause)) => Poll::Ready(Err(cause)),
+            Poll::Pending => Poll::Pending,
+        }
+    }
+}
+
+#[pin_project]
+struct MemAcceptBiFuture<'a, Req, Res>(
+    #[pin] futures::stream::Next<'a, mpsc::Receiver<MemSocket<Req, Res>>>,
+);
+
+impl<'a, Req, Res> Future for MemAcceptBiFuture<'a, Req, Res> {
+    type Output = result::Result<MemSocket<Req, Res>, PeerDropped>;
+
+    fn poll(
+        self: Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Self::Output> {
+        match self.project().0.poll_unpin(cx) {
+            Poll::Ready(Some(socket)) => Poll::Ready(Ok(socket)),
+            Poll::Ready(None) => Poll::Ready(Err(PeerDropped)),
+            Poll::Pending => Poll::Pending,
+        }
+    }
+}
+
+impl<Req: Send + 'static, Res: Send + 'static> Channel<Req, Res> for MemChannel<Req, Res> {
     type Sink = mpsc::Sender<Req>;
 
     type Stream = WrapNoError<mpsc::Receiver<Res>>;
-
-    type OpenBiError = mpsc::SendError;
-
-    type AcceptBiError = PeerDropped;
 
     type SendError = mpsc::SendError;
 
     type RecvError = NoError;
 
-    type OpenBiFuture<'a> = BoxFuture<'a, result::Result<MemSocket<Req, Res>, Self::OpenBiError>>;
+    type OpenBiError = mpsc::SendError;
+
+    type OpenBiFuture<'a> = MemOpenBiFuture<'a, Req, Res>;
 
     fn open_bi(&mut self) -> Self::OpenBiFuture<'_> {
         let (local_send, remote_recv) = mpsc::channel::<Req>(1);
         let (remote_send, local_recv) = mpsc::channel::<Res>(1);
-        async {
-            let remote_recv = WrapNoError { inner: remote_recv };
-            let local_recv = WrapNoError { inner: local_recv };
-            // try to send the remote end
-            self.sink.send((remote_send, remote_recv)).await?;
-            // keep the local end and return it
-            Ok((local_send, local_recv))
-        }.boxed()
+        let remote_recv = WrapNoError { inner: remote_recv };
+        let local_recv = WrapNoError { inner: local_recv };
+        let inner = self.sink.send((remote_send, remote_recv));
+        MemOpenBiFuture::new(inner, (local_send, local_recv))
     }
 
-    type AcceptBiFuture<'a> = BoxFuture<'a, result::Result<MemSocket<Req, Res>, Self::AcceptBiError>>;
+    type AcceptBiError = PeerDropped;
+
+    type AcceptBiFuture<'a> = MemAcceptBiFuture<'a, Req, Res>;
 
     fn accept_bi(&mut self) -> Self::AcceptBiFuture<'_> {
-        async {
-            self.stream.next().await.ok_or(PeerDropped)
-        }.boxed()
+        MemAcceptBiFuture(self.stream.next())
     }
 }
 
