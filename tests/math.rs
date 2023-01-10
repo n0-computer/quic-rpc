62ea8555aa5f8da0b3f2fb4975aa1c2304756cd4
Ruediger Klaehn <rklaehn@protonmail.com>
2023-01-10 10:32:19 +0200

    fmt, also try out s2n-quic native crypto
    (does not make a difference regarding the bug)

--- tests/math.rs
+++ tests/math.rs
@@ -300,10 +300,14 @@ where
         let (send, recv) = client.bidi(Multiply(2)).await?;
         let handle = tokio::task::spawn(async move {
             let requests = futures::stream::iter((0..n).map(MultiplyUpdate));
-            requests.map(|r| {
-                // println!("u {:?}", r);
-                r
-            }).map(Ok).forward(send).await?;
+            requests
+                .map(|r| {
+                    // println!("u {:?}", r);
+                    r
+                })
+                .map(Ok)
+                .forward(send)
+                .await?;
             anyhow::Result::<()>::Ok(())
         });
         let mut sum = 0;
