90782e16295757d51e876f7a4a64f1ce940ca8ef
Ruediger Klaehn <rklaehn@protonmail.com>
2023-02-17 13:56:44 +0200

    refactor: get rid of libp2p deps for now

--- tests/s2n_quic.rs
+++ tests/s2n_quic.rs
@@ -2,7 +2,6 @@
 mod math;
 use std::{net::SocketAddr, path::Path};
 
-use libp2p_core::identity::ed25519::Keypair;
 use math::*;
 mod util;
 use quic_rpc::{RpcClient, RpcServer};
@@ -18,35 +17,35 @@ fn run_server(server: s2n_quic::Server) -> JoinHandle<anyhow::Result<()>> {
     })
 }
 
-async fn make_client_and_server_rustls() -> anyhow::Result<(
-    s2n_quic::Client,
-    s2n_quic::Server,
-    s2n_quic::client::Connect,
-)> {
-    let client_keypair = libp2p_core::identity::Keypair::Ed25519(Keypair::generate());
-    let client_config = libp2p_tls::make_client_config(&client_keypair, None)?;
-    let client_tls = s2n_quic::provider::tls::rustls::Client::from(client_config);
+// async fn make_client_and_server_rustls() -> anyhow::Result<(
+//     s2n_quic::Client,
+//     s2n_quic::Server,
+//     s2n_quic::client::Connect,
+// )> {
+//     let client_keypair = libp2p_core::identity::Keypair::Ed25519(Keypair::generate());
+//     let client_config = libp2p_tls::make_client_config(&client_keypair, None)?;
+//     let client_tls = s2n_quic::provider::tls::rustls::Client::from(client_config);
 
-    let client = s2n_quic::Client::builder()
-        .with_tls(client_tls)?
-        .with_io("0.0.0.0:0")?
-        .start()
-        .map_err(|e| anyhow::anyhow!("{:?}", e))?;
+//     let client = s2n_quic::Client::builder()
+//         .with_tls(client_tls)?
+//         .with_io("0.0.0.0:0")?
+//         .start()
+//         .map_err(|e| anyhow::anyhow!("{:?}", e))?;
 
-    let server_keypair = libp2p_core::identity::Keypair::Ed25519(Keypair::generate());
-    let server_config = libp2p_tls::make_server_config(&server_keypair)?;
-    let server_tls = s2n_quic::provider::tls::rustls::Server::from(server_config);
+//     let server_keypair = libp2p_core::identity::Keypair::Ed25519(Keypair::generate());
+//     let server_config = libp2p_tls::make_server_config(&server_keypair)?;
+//     let server_tls = s2n_quic::provider::tls::rustls::Server::from(server_config);
 
-    let server = s2n_quic::Server::builder()
-        .with_tls(server_tls)?
-        .with_io("0.0.0.0:4433")?
-        .start()
-        .map_err(|e| anyhow::anyhow!("{:?}", e))?;
+//     let server = s2n_quic::Server::builder()
+//         .with_tls(server_tls)?
+//         .with_io("0.0.0.0:4433")?
+//         .start()
+//         .map_err(|e| anyhow::anyhow!("{:?}", e))?;
 
-    let addr: SocketAddr = "127.0.0.1:4433".parse()?;
-    let connect = s2n_quic::client::Connect::new(addr).with_server_name("localhost");
-    Ok((client, server, connect))
-}
+//     let addr: SocketAddr = "127.0.0.1:4433".parse()?;
+//     let connect = s2n_quic::client::Connect::new(addr).with_server_name("localhost");
+//     Ok((client, server, connect))
+// }
 
 async fn make_client_and_server_builtin() -> anyhow::Result<(
     s2n_quic::Client,
