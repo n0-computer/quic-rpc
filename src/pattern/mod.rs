0338e43a43a56f8c2ea90a6e6321a4f2e25b4057
Ruediger Klaehn <rklaehn@protonmail.com>
2024-04-10 14:04:33 +0300

    add more docs about what an interaction pattern is

--- src/pattern/mod.rs
+++ src/pattern/mod.rs
@@ -1,4 +1,9 @@
+//! Predefined interaction patterns.
 //!
+//! An interaction pattern can be as simple as an rpc call or something more
+//! complex such as bidirectional streaming.
+//!
+//! Each pattern defines different associated message types for the interaction.
 pub mod bidi_streaming;
 pub mod client_streaming;
 pub mod rpc;
