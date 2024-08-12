2221339d5e98cb2f8952b303c5ead24c8030f1f8
dignifiedquire <me@dignifiedquire.com>
2024-08-12 15:51:25 +0200

    deps: Upgrade to Quinn 0.11 and Rustls 0.23
    This upgrades the Quinn and Rustls dependencies.
    
    Currently we have no released iroh-quinn yet for those versions so it
    points back to upstream Quinn.  We should fix this before merging.

--- quic-rpc-derive/tests/smoke.rs
+++ quic-rpc-derive/tests/smoke.rs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 use quic_rpc_derive::rpc_requests;
 use serde::{Deserialize, Serialize};
 
@@ -78,51 +77,3 @@ fn compile_fail() {
     let t = trybuild::TestCases::new();
     t.compile_fail("tests/compile_fail/*.rs");
 }
-||||||| parent of 66d32e2 (deps: Upgrade to Quinn 0.11 and Rustls 0.23)
-=======
-use quic_rpc::pattern::{bidi_streaming, server_streaming};
-use quic_rpc_derive::rpc_requests;
-use serde::{Deserialize, Serialize};
-
-#[test]
-fn simple() {
-    #[derive(Debug, Serialize, Deserialize)]
-    struct RpcRequest;
-
-    #[derive(Debug, Serialize, Deserialize)]
-    struct ServerStreamingRequest;
-
-    #[derive(Debug, Serialize, Deserialize)]
-    struct ClientStreamingRequest;
-
-    #[derive(Debug, Serialize, Deserialize)]
-    struct BidiStreamingRequest;
-
-    #[rpc_requests(Service)]
-    #[derive(Debug, Serialize, Deserialize, derive_more::From, derive_more::TryInto)]
-    enum Request {
-        #[rpc(response=())]
-        Rpc(RpcRequest),
-        #[server_streaming(response=())]
-        ServerStreaming(ServerStreamingRequest),
-        #[bidi_streaming(update=(), response=())]
-        BidiStreaming(BidiStreamingRequest),
-        #[client_streaming(update=(), response=())]
-        ClientStreaming(ClientStreamingRequest),
-        GenericUpdate(()),
-    }
-
-    #[derive(Debug, Serialize, Deserialize, derive_more::From, derive_more::TryInto)]
-    enum Response {
-        Void(()),
-    }
-
-    #[derive(Debug, Clone)]
-    struct Service;
-
-    impl quic_rpc::Service for Service {
-        type Req = Request;
-        type Res = Response;
-    }
-}
->>>>>>> 66d32e2 (deps: Upgrade to Quinn 0.11 and Rustls 0.23)
