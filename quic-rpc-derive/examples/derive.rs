c28ae8ba5b0ffae47f74d86cd2fa3131e7a4e939
Ruediger Klaehn <rklaehn@protonmail.com>
2025-03-16 13:00:50 +0200

    WIP macros

---
+++ quic-rpc-derive/examples/derive.rs
@@ -0,0 +1,251 @@
+use std::{
+    collections::BTreeMap,
+    marker::PhantomData,
+    net::{Ipv4Addr, SocketAddr, SocketAddrV4},
+    sync::Arc,
+};
+
+use n0_future::task::AbortOnDropHandle;
+use quic_rpc::{
+    channel::{mpsc, none::NoReceiver, oneshot},
+    rpc::{listen, Handler},
+    util::{make_client_endpoint, make_server_endpoint},
+    Channels, LocalMpscChannel, Service, ServiceRequest, ServiceSender, WithChannels,
+};
+use quic_rpc_derive::rpc_protocol;
+use serde::{Deserialize, Serialize};
+use tracing::info;
+
+/// A simple storage service, just to try it out
+#[derive(Debug, Clone, Copy)]
+struct StorageService;
+
+impl Service for StorageService {}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Get {
+    key: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct List;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Set {
+    key: String,
+    value: String,
+}
+
+// mod manual {
+//     use super::*;
+//     impl Channels<StorageService> for Get {
+//         type Rx = NoReceiver;
+//         type Tx = oneshot::Sender<Option<String>>;
+//     }
+
+//     impl Channels<StorageService> for List {
+//         type Rx = NoReceiver;
+//         type Tx = mpsc::Sender<String>;
+//     }
+
+//     impl Channels<StorageService> for Set {
+//         type Rx = NoReceiver;
+//         type Tx = oneshot::Sender<()>;
+//     }
+// }
+
+#[derive(derive_more::From, Serialize, Deserialize)]
+#[rpc_protocol(StorageMessage, StorageService)]
+enum StorageProtocol {
+    #[tx(oneshot::Sender<Option<String>>)]
+    Get(Get),
+    #[tx(oneshot::Sender<()>)]
+    Set(Set),
+    #[tx(mpsc::Sender<String>)]
+    List(List),
+}
+
+struct StorageActor {
+    recv: tokio::sync::mpsc::Receiver<StorageMessage>,
+    state: BTreeMap<String, String>,
+}
+
+impl StorageActor {
+    pub fn local() -> StorageApi {
+        let (tx, rx) = tokio::sync::mpsc::channel(1);
+        let actor = Self {
+            recv: rx,
+            state: BTreeMap::new(),
+        };
+        tokio::spawn(actor.run());
+        let local = LocalMpscChannel::<StorageMessage, StorageService>::from(tx);
+        StorageApi {
+            inner: local.into(),
+        }
+    }
+
+    async fn run(mut self) {
+        while let Some(msg) = self.recv.recv().await {
+            self.handle(msg).await;
+        }
+    }
+
+    async fn handle(&mut self, msg: StorageMessage) {
+        match msg {
+            StorageMessage::Get(get) => {
+                info!("get {:?}", get);
+                let WithChannels { tx, inner, .. } = get;
+                tx.send(self.state.get(&inner.key).cloned()).await.ok();
+            }
+            StorageMessage::Set(set) => {
+                info!("set {:?}", set);
+                let WithChannels { tx, inner, .. } = set;
+                self.state.insert(inner.key, inner.value);
+                tx.send(()).await.ok();
+            }
+            StorageMessage::List(list) => {
+                info!("list {:?}", list);
+                let WithChannels { mut tx, .. } = list;
+                for (key, value) in &self.state {
+                    if tx.send(format!("{key}={value}")).await.is_err() {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
+struct StorageApi {
+    inner: ServiceSender<StorageMessage, StorageProtocol, StorageService>,
+}
+
+impl StorageApi {
+    pub fn connect(endpoint: quinn::Endpoint, addr: SocketAddr) -> anyhow::Result<StorageApi> {
+        Ok(StorageApi {
+            inner: ServiceSender::Remote(endpoint, addr, PhantomData),
+        })
+    }
+
+    pub fn listen(&self, endpoint: quinn::Endpoint) -> anyhow::Result<AbortOnDropHandle<()>> {
+        match &self.inner {
+            ServiceSender::Local(local, _) => {
+                let local = LocalMpscChannel::from(local.clone());
+                let fun: Handler<StorageProtocol> = Arc::new(move |msg, _, tx| {
+                    let local = local.clone();
+                    Box::pin(async move {
+                        match msg {
+                            StorageProtocol::Get(msg) => {
+                                local.send((msg, tx)).await?;
+                            }
+                            StorageProtocol::Set(msg) => {
+                                local.send((msg, tx)).await?;
+                            }
+                            StorageProtocol::List(msg) => {
+                                local.send((msg, tx)).await?;
+                            }
+                        };
+                        Ok(())
+                    })
+                });
+                Ok(listen(endpoint, fun))
+            }
+            ServiceSender::Remote(_, _, _) => {
+                Err(anyhow::anyhow!("cannot listen on a remote service"))
+            }
+        }
+    }
+
+    pub async fn get(&self, key: String) -> anyhow::Result<oneshot::Receiver<Option<String>>> {
+        let msg = Get { key };
+        match self.inner.request().await? {
+            ServiceRequest::Local(request, _) => {
+                let (tx, rx) = oneshot::channel();
+                request.send((msg, tx)).await?;
+                Ok(rx)
+            }
+            ServiceRequest::Remote(request) => {
+                let (rx, _tx) = request.write(msg).await?;
+                Ok(rx.into())
+            }
+        }
+    }
+
+    pub async fn list(&self) -> anyhow::Result<mpsc::Receiver<String>> {
+        let msg = List;
+        match self.inner.request().await? {
+            ServiceRequest::Local(request, _) => {
+                let (tx, rx) = mpsc::channel(10);
+                request.send((msg, tx)).await?;
+                Ok(rx)
+            }
+            ServiceRequest::Remote(request) => {
+                let (rx, _tx) = request.write(msg).await?;
+                Ok(rx.into())
+            }
+        }
+    }
+
+    pub async fn set(&self, key: String, value: String) -> anyhow::Result<oneshot::Receiver<()>> {
+        let msg = Set { key, value };
+        match self.inner.request().await? {
+            ServiceRequest::Local(request, _) => {
+                let (tx, rx) = oneshot::channel();
+                request.send((msg, tx)).await?;
+                Ok(rx)
+            }
+            ServiceRequest::Remote(request) => {
+                let (rx, _tx) = request.write(msg).await?;
+                Ok(rx.into())
+            }
+        }
+    }
+}
+
+async fn local() -> anyhow::Result<()> {
+    let api = StorageActor::local();
+    api.set("hello".to_string(), "world".to_string())
+        .await?
+        .await?;
+    let value = api.get("hello".to_string()).await?.await?;
+    let mut list = api.list().await?;
+    while let Some(value) = list.recv().await? {
+        println!("list value = {:?}", value);
+    }
+    println!("value = {:?}", value);
+    Ok(())
+}
+
+async fn remote() -> anyhow::Result<()> {
+    let port = 10113;
+    let (server, cert) =
+        make_server_endpoint(SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, port).into())?;
+    let client =
+        make_client_endpoint(SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, 0).into(), &[&cert])?;
+    let store = StorageActor::local();
+    let handle = store.listen(server)?;
+    let api = StorageApi::connect(client, SocketAddrV4::new(Ipv4Addr::LOCALHOST, port).into())?;
+    api.set("hello".to_string(), "world".to_string())
+        .await?
+        .await?;
+    api.set("goodbye".to_string(), "world".to_string())
+        .await?
+        .await?;
+    let value = api.get("hello".to_string()).await?.await?;
+    println!("value = {:?}", value);
+    let mut list = api.list().await?;
+    while let Some(value) = list.recv().await? {
+        println!("list value = {:?}", value);
+    }
+    drop(handle);
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt().init();
+    println!("Local use");
+    local().await?;
+    println!("Remote use");
+    remote().await?;
+    Ok(())
+}
--- quic-rpc-derive/src/lib.rs
+++ quic-rpc-derive/src/lib.rs
@@ -1,6 +1,33 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, Data, DeriveInput, Fields, Type};
+use syn::{
+    parse::{Parse, ParseStream},
+    parse_macro_input,
+    spanned::Spanned,
+    Data, DeriveInput, Fields, Ident, Token, Type,
+};
+
+struct TxArgs {
+    tx_type: Type,
+}
+
+impl Parse for TxArgs {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let tx_type: Type = input.parse()?;
+        Ok(TxArgs { tx_type })
+    }
+}
+
+struct RxArgs {
+    rx_type: Type,
+}
+
+impl Parse for RxArgs {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let rx_type: Type = input.parse()?;
+        Ok(RxArgs { rx_type })
+    }
+}
 
 /// A macro to generate a Message enum from a Protocol enum.
 ///
@@ -132,5 +159,244 @@ pub fn message_enum(attr: TokenStream, item: TokenStream) -> TokenStream {
         }
     };
 
+    expanded.into()
+}
+
+/// A macro to generate a Message enum from a Protocol enum and implement Channels for each variant.
+/// 
+/// This macro:
+/// 1. Takes a Protocol enum and generates a corresponding Message enum with variants wrapped in WithChannels
+/// 2. Implements the Channels trait for each variant's inner type based on #[tx(...)] and #[rx(...)] attributes
+///
+/// Channel receiver (rx) defaults to NoReceiver if not specified.
+///
+/// # Example
+///
+/// ```
+/// #[rpc_protocol(StorageMessage, StorageService)]
+/// enum StorageProtocol {
+///     #[tx(oneshot::Sender<Option<String>>)]
+///     Get(Get),
+///     #[tx(oneshot::Sender<()>)]
+///     Set(Set),
+///     #[tx(mpsc::Sender<String>)]
+///     List(List),
+///     #[rx(mpsc::Receiver<Query>)]
+///     #[tx(mpsc::Sender<Result>)]
+///     Query(Query),
+/// }
+/// ```
+#[proc_macro_attribute]
+pub fn rpc_protocol(attr: TokenStream, item: TokenStream) -> TokenStream {
+    // Parse the input
+    let mut input = parse_macro_input!(item as DeriveInput);
+    let attr_args = parse_macro_input!(attr as syn::AttributeArgs);
+    
+    // Extract message enum name and service type from attributes
+    if attr_args.len() != 2 {
+        return syn::Error::new(
+            proc_macro2::Span::call_site(),
+            "Expected two arguments: message_enum_name and service_type",
+        )
+        .to_compile_error()
+        .into();
+    }
+    
+    let message_enum_name = match &attr_args[0] {
+        syn::NestedMeta::Meta(syn::Meta::Path(path)) => {
+            if let Some(ident) = path.get_ident() {
+                ident
+            } else {
+                return syn::Error::new(
+                    proc_macro2::Span::call_site(),
+                    "First argument must be an identifier for the message enum name",
+                ).to_compile_error().into();
+            }
+        },
+        _ => {
+            return syn::Error::new(
+                proc_macro2::Span::call_site(),
+                "First argument must be an identifier for the message enum name",
+            ).to_compile_error().into();
+        }
+    };
+    
+    let service_type = match &attr_args[1] {
+        syn::NestedMeta::Meta(syn::Meta::Path(path)) => {
+            if let Some(ident) = path.get_ident() {
+                ident
+            } else {
+                return syn::Error::new(
+                    proc_macro2::Span::call_site(),
+                    "Second argument must be an identifier for the service type",
+                ).to_compile_error().into();
+            }
+        },
+        _ => {
+            return syn::Error::new(
+                proc_macro2::Span::call_site(),
+                "Second argument must be an identifier for the service type",
+            ).to_compile_error().into();
+        }
+    };
+    
+    // Extract the variants from the enum
+    let data_enum = match &mut input.data {
+        Data::Enum(data_enum) => data_enum,
+        _ => {
+            return syn::Error::new(
+                input.ident.span(),
+                "This macro only works on enums",
+            ).to_compile_error().into();
+        }
+    };
+    
+    // Generate variants for the message enum
+    let message_variants = data_enum.variants.iter().map(|variant| {
+        let variant_name = &variant.ident;
+        
+        // Extract the inner type from the variant
+        let inner_type = match &variant.fields {
+            Fields::Unnamed(fields) if fields.unnamed.len() == 1 => {
+                if let Type::Path(type_path) = &fields.unnamed.first().unwrap().ty {
+                    if let Some(last_segment) = type_path.path.segments.last() {
+                        &last_segment.ident
+                    } else {
+                        return syn::Error::new(
+                            variant.ident.span(),
+                            "Unable to extract type name from variant",
+                        ).to_compile_error();
+                    }
+                } else {
+                    return syn::Error::new(
+                        variant.ident.span(),
+                        "Variant must contain exactly one unnamed field of a simple type",
+                    ).to_compile_error();
+                }
+            },
+            _ => {
+                return syn::Error::new(
+                    variant.ident.span(),
+                    "Variant must contain exactly one unnamed field",
+                ).to_compile_error();
+            }
+        };
+        
+        quote! {
+            #variant_name(WithChannels<#inner_type, #service_type>)
+        }
+    });
+    
+    // Storage for additional implementations
+    let mut channel_impls = Vec::new();
+    
+    // Process each variant
+    for variant in &mut data_enum.variants {
+        // Extract the inner type from the variant
+        let inner_type = match &variant.fields {
+            Fields::Unnamed(fields) if fields.unnamed.len() == 1 => {
+                if let Type::Path(type_path) = &fields.unnamed.first().unwrap().ty {
+                    if let Some(last_segment) = type_path.path.segments.last() {
+                        last_segment.ident.clone()
+                    } else {
+                        return syn::Error::new(
+                            variant.ident.span(),
+                            "Unable to extract type name from variant",
+                        ).to_compile_error().into();
+                    }
+                } else {
+                    return syn::Error::new(
+                        variant.ident.span(),
+                        "Variant must contain exactly one unnamed field of a simple type",
+                    ).to_compile_error().into();
+                }
+            },
+            _ => {
+                return syn::Error::new(
+                    variant.ident.span(),
+                    "Variant must contain exactly one unnamed field",
+                ).to_compile_error().into();
+            }
+        };
+        
+        // Default rx to NoReceiver
+        let mut rx_type = quote! { NoReceiver };
+        let mut tx_type = None;
+        
+        // Extract and remove rx attributes
+        let mut rx_attr = None;
+        variant.attrs.retain(|attr| {
+            if attr.path.is_ident("rx") {
+                rx_attr = Some(attr.clone());
+                false
+            } else {
+                true
+            }
+        });
+        
+        // Extract and remove tx attributes
+        let mut tx_attr = None;
+        variant.attrs.retain(|attr| {
+            if attr.path.is_ident("tx") {
+                tx_attr = Some(attr.clone());
+                false
+            } else {
+                true
+            }
+        });
+        
+        // Parse rx attribute if found
+        if let Some(attr) = rx_attr {
+            match attr.parse_args::<RxArgs>() {
+                Ok(args) => {
+                    rx_type = quote! { #args.rx_type };
+                },
+                Err(e) => return e.to_compile_error().into(),
+            }
+        }
+        
+        // Parse tx attribute - required
+        let tx_attr = match tx_attr {
+            Some(attr) => attr,
+            None => {
+                return syn::Error::new(
+                    variant.ident.span(),
+                    format!("Missing #[tx(...)] attribute for variant {}", variant.ident),
+                ).to_compile_error().into();
+            }
+        };
+        
+        match tx_attr.parse_args::<TxArgs>() {
+            Ok(args) => {
+                tx_type = Some(quote! { #args.tx_type });
+            },
+            Err(e) => return e.to_compile_error().into(),
+        }
+        
+        // Generate the Channel implementation
+        let tx_type = tx_type.unwrap();
+        channel_impls.push(quote! {
+            impl Channels<#service_type> for #inner_type {
+                type Rx = #rx_type;
+                type Tx = #tx_type;
+            }
+        });
+    }
+    
+    // Generate the complete output
+    let expanded = quote! {
+        // Keep the original protocol enum
+        #input
+        
+        // Generate the message enum
+        #[derive(derive_more::From)]
+        enum #message_enum_name {
+            #(#message_variants),*
+        }
+        
+        // Implement Channels for each type
+        #(#channel_impls)*
+    };
+    
     expanded.into()
 }
